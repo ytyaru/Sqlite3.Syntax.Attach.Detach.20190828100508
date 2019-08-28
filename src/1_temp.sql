992136615970f2610b0fce569b41e3ac972b1b59
ytyaru <ytyaru@outlook.jp>
2019-08-28 11:04:08 +0900

    一時テーブル略記でない版

--- src/1_temp.sql
+++ src/1_temp.sql
@@ -1,7 +1,11 @@
 create temp table T(A int);
+create temporary table U(A int);
 select sql from temp.sqlite_master;
 select sql from temp.sqlite_temp_master;
 
 insert into temp.T values(0);
 select * from temp.T;
+
+insert into temp.U values(0);
+select * from temp.U;
 
